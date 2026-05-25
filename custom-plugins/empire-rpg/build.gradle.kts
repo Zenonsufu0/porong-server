0b19ecb5b65a830a352193a03ba93cd48f3aeef6
Zenonsufu0 <zenonsufu0@gmail.com>
2026-05-25 20:34:25 +0900

    §6-11 WorldGuardSafeZoneService —   Maven 의존성, pvp DENY 안전구역 차단 실구현

--- custom-plugins/empire-rpg/build.gradle.kts
+++ custom-plugins/empire-rpg/build.gradle.kts
@@ -17,6 +17,7 @@ repositories {
 
     maven("https://repo.papermc.io/repository/maven-public/")
     maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
+    maven("https://maven.enginehub.org/repo/")
 }
 
 dependencies {
@@ -34,9 +35,12 @@ dependencies {
     // IridiumSkyblock 4.x — 로컬 JAR (런타임은 서버 플러그인에서 제공)
     compileOnly(files("../../server/plugins/IridiumSkyblock-4.1.4.jar"))
 
-    // WorldGuard + WorldEdit 7.x — 로컬 JAR (softdepend, 런타임은 서버 플러그인에서 제공)
-    compileOnly(files("../../server/plugins/worldguard-bukkit-7.0.15.jar"))
-    compileOnly(files("../../server/plugins/worldedit-bukkit-7.4.2.jar"))
+    // WorldGuard + WorldEdit 7.x — EngineHub Maven (softdepend, 런타임은 서버 플러그인에서 제공)
+    // strictly 버전 충돌을 막기 위해 transitive 의존성 제외 후 필요한 것만 명시
+    compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.15-SNAPSHOT") { isTransitive = false }
+    compileOnly("com.sk89q.worldguard:worldguard-core:7.0.15-SNAPSHOT") { isTransitive = false }
+    compileOnly("com.sk89q.worldedit:worldedit-bukkit:7.4.2-SNAPSHOT") { isTransitive = false }
+    compileOnly("com.sk89q.worldedit:worldedit-core:7.4.2-SNAPSHOT") { isTransitive = false }
 
     // Gson — Paper가 런타임에 제공하므로 compileOnly
     compileOnly("com.google.code.gson:gson:2.10.1")
